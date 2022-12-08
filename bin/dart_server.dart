7435e55828b1fbe75cc5d54362b92256f4ccbb6b
EwigLife <zain@ewiglife.com>
2022-12-09 04:04:40 +0500

    sending data to json

--- bin/dart_server.dart
+++ bin/dart_server.dart
@@ -1,11 +1,14 @@
+import 'dart:convert';
 import 'dart:io';
+import 'package:mysql_client/mysql_client.dart';
 import 'package:shelf/shelf_io.dart' as io;
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
 final app = Router()
   ..get('/', _rootHandler)
-  ..get('/echo/<message>', _echoHandler);
+  ..get('/echo/<message>', _echoHandler)
+  ..get('/sql', _sqlHandler);
 
 Response _rootHandler(Request request) {
   return Response.ok('Hello World!');
@@ -16,13 +19,27 @@ Response _echoHandler(Request request) {
   return Response.ok('$message\n');
 }
 
-void main(List<String> arguments) async {
-  // app.get('/hello', (Request request) {
-  //   var json = {'message': 'Hello World!'};
-  //   return Response.ok(jsonEncode(json),
-  //       headers: {'Content-Type': 'application/json'});
-  // });
+Future<Response> _sqlHandler(Request request) async {
+  final settings = await MySQLConnection.createConnection(
+    host: '127.0.0.1',
+    port: 3306,
+    userName: 'dart',
+    password: 'Dart_123',
+    databaseName: 'dart',
+  );
+  await settings.connect();
+  print('connected');
+  final results = await settings.execute('SELECT * FROM users');
+  var list = [];
+  for (var row in results.rows) {
+    var map = row.assoc();
+    list.add(jsonEncode(map));
+  }
+  return Response.ok('db results: $list\n',
+      headers: {'Content-Type': 'application/json'});
+}
 
+void main(List<String> arguments) async {
   final ip = InternetAddress.anyIPv4;
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
   final handler = const Pipeline().addMiddleware(logRequests()).addHandler(app);
