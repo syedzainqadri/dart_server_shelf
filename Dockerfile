54f29de9dd93b2f2b17d18ff385a1b953ad7a24d
EwigLife <96788793+syedzainqadri@users.noreply.github.com>
2023-02-08 15:48:30 +0500

    Docker container created

--- Dockerfile
+++ Dockerfile
@@ -25,18 +25,15 @@
 # CMD ["./build/server"]
 
 
-FROM dart:stable
-
+FROM dart:stable as builder
 COPY . /dart_server
-
 WORKDIR /dart_server
-
 COPY pubspec.* ./
 RUN dart pub get
-
 RUN mkdir build
-
 RUN dart compile exe ./bin/server.dart -o ./build/server
-
-CMD [ "./build/server" ]
+FROM debian:buster-slim
+COPY --from=builder /dart_server/build /bin
+COPY --from=builder /runtime/ /
+CMD [ "server" ]
 
--- prisma/development.dart
+++ prisma/development.dart
@@ -2,8 +2,8 @@ import 'package:orm/configure.dart';
 
 /// Prisma development environment configurator.
 void configurator(PrismaDevelopment development) {
-  development.override(
-      'DATABASE_URL', r'mysql://dart:Dart_123@localhost:3306/dart');
+  development.override('DATABASE_URL',
+      r'mysql://doadmin:AVNS_x1TzzXT75yePLxPsDv8@db-mysql-nyc1-39825-do-user-13057874-0.b.db.ondigitalocean.com:25060/defaultdb');
 
   // You can override environment variable example:
   // development.override('DEBUG', 'true');
