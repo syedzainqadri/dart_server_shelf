e64eaad63335abfb4bde56229f28f75814a32b36
EwigLife <96788793+syedzainqadri@users.noreply.github.com>
2023-05-08 04:02:10 +0500

    updated prisma_configurator for the new db

--- lib/prisma_configurator.dart
+++ lib/prisma_configurator.dart
@@ -5,5 +5,5 @@ import 'package:orm/configure.dart';
 /// **NOTE**: The function name must be configurePrisma.
 void configurePrisma(PrismaEnvironment environment) {
   environment['DATABASE_URL'] =
-      r'mysql://mysql://admin:Ghrounda_123@ghrounda-database.cnlt1am4mrxg.us-east-2.rds.amazonaws.com:3306/ghrounda';
+      r'mysql://admin:Ghrounda_123@ghrounda-database.cnlt1am4mrxg.us-east-2.rds.amazonaws.com:3306/ghrounda';
 }
