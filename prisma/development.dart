065b2fe21f6ab9a996bb9e071e82739616e17965
EwigLife <96788793+syedzainqadri@users.noreply.github.com>
2023-05-19 04:32:26 +0500

    category issue fixed database changed

--- prisma/development.dart
+++ prisma/development.dart
@@ -3,7 +3,7 @@ import 'package:orm/configure.dart';
 /// Prisma development environment configurator.
 void configurator(PrismaDevelopment development) {
   development.override('DATABASE_URL',
-      r'mysql://admin:Ghrounda_123@ghrounda-database.cnlt1am4mrxg.us-east-2.rds.amazonaws.com:3306/ghrounda');
+      r'mysql://ghroundadb:xB8O1_QY;6FV;z(V0x:~LZ9n,zvG2F1!@ls-ba76fa0894e6f74cd1b6ced47e42e03f256d4e73.cxipqwqmjllh.ap-southeast-1.rds.amazonaws.com:3306/dbghrounda');
 
   // You can override environment variable example:
   // development.override('DEBUG', 'true');
